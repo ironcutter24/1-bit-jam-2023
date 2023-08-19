10ee011423b11691327504487d0e8a93e259766f
ironcutter24 <ironcutter24@gmail.com>
2023-08-19 10:50:21 +0200

    Level 00

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 
 const MOVE_SPEED = 60.0
-const JUMP_VELOCITY = -220.0
+const JUMP_VELOCITY = -250.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
